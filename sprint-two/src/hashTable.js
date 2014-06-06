a95351be01c10c066d488dff1e29591cc88aa9f1
Austen Talbot <austentalbot@gmail.com>
2014-06-05 22:18:05 -0700

    Completed hash table.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,28 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var obj={};
+  obj[k]=v;
+  if (this._storage.get(i)===undefined) {
+    this._storage.set(i, obj);
+  } else {
+    this._storage.set(i, _.extend(this._storage.get(i), obj));
+  }
+  console.log(this._storage.get(i));
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var obj= this._storage.get(i);
+  return obj[k];
 
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var obj={};
+  obj[k]=null;
+  this._storage.set(i, _.extend(this._storage.get(i), obj));
 };
 
 
