05a2c35c267f7439dacfe722915bebb732de0290
Austen Talbot <austentalbot@gmail.com>
2014-06-07 10:57:09 -0700

    Completed extra credit for tree. Added parent and removeFromParent.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,7 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  newTree.parent=null;
   _.extend(newTree, treeMethods);
   return newTree;
 };
@@ -13,6 +14,7 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value){
   var node=makeTree();
+  node.parent=this;
   node.value=value;
   if (this.children===undefined) {
     this.children=[];
@@ -20,6 +22,34 @@ treeMethods.addChild = function(value){
   this.children.push(node);
 
 };
+
+treeMethods.removeFromParent= function(target) {
+  // find node
+  var emancipated=null;
+  var dig=function(target, place) {
+    _.each(place, function(val, index) {
+      if (Array.isArray(val['children'])) {
+        dig(target, val['children']);
+
+      } 
+      if (val['value']===target) {
+        emancipated=val;
+        //remove from parent
+        emancipated.parent.children.splice(index, 1);
+        //remove parent reference
+        emancipated.parent=null;
+      }
+    });
+  };
+  dig(target, this.children);
+  if (emancipated!==null) {
+    return emancipated;
+  }
+
+  // set parent for this value's node to null
+  // remove this node from the parent's children array
+  // return the new, independent tree
+}
 
 treeMethods.contains = function(target){
   /*
