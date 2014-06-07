39d0555e9815aa5343535cbbf4e0877a606a57cd
Austen Talbot <austentalbot@gmail.com>
2014-06-07 16:00:17 -0700

    Updated all files to reflect current status of work.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,22 +1,89 @@
 var Graph = function(){
+  this.nodes=[];
+  this.edges=[];
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+
+  this.addEdge(newNode, toNode);
+  this.nodes.push(newNode);
+
+  // create edge when only two nodes
+  if (this.nodes.length===2) {
+    this.addEdge(this.nodes[0], this.nodes[1]);
+  }
 };
 
 Graph.prototype.contains = function(node){
+  var found=false;
+  _.each(this.nodes, function(val){
+    if (val===node) {
+      found=true;
+    }
+  });
+  return found;
 };
 
 Graph.prototype.removeNode = function(node){
+  var returnNodes=[];
+  _.each(this.nodes, function(val){
+    if (val!==node) {
+      returnNodes.push(val);
+    }
+  });
+  this.nodes=returnNodes;  
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+  var found=false;
+  _.each(this.edges, function(edge){
+    if (edge['from']===fromNode && edge['to']===toNode) {
+      found=true;
+    }
+  });  
+  return found;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  var edge={};
+  edge.to=toNode;
+  edge.from=fromNode;
+  this.edges.push(edge);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var returnEdges=[];
+  var from=false;
+  var to=false;
+
+  // remove edge
+  _.each(this.edges, function(edge){
+    if (edge['from']!==fromNode || edge['to']!==toNode) {
+      returnEdges.push(edge);
+    }
+  });
+      
+
+  this.edges=returnEdges;
+
+  //test if from and to nodes have any other edges
+  _.each(returnEdges, function(edge){
+    if (edge['from']===fromNode || edge['to']===fromNode) {
+      from=true;
+    }
+    if (edge['from']===toNode || edge['to']===toNode) {
+      to=true;
+    }
+  });
+
+  console.log(returnEdges);
+
+  if (from===false) {
+    this.removeNode(fromNode);
+  }
+  if (to===false) {
+    this.removeNode(toNode);
+  }
 };
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,34 +1,105 @@
-var HashTable = function(){
-  this._limit = 8;
+var HashTable = function(limit){
+  if (limit===undefined) {
+    this._limit = 8;
+  } else {
+    this._limit=limit;
+  }
+  this._count=0;
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
+  // get index
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var obj={};
-  obj[k]=v;
-  if (this._storage.get(i)===undefined) {
-    this._storage.set(i, obj);
+  // create array and populate with key and value
+  var tuple = [k, v];
+
+  // if index is null set index with a new array populated with tuple;
+  // else assign array at index to bucket and push the newly created tuple to array
+  // set index with new array
+  if (this._storage.get(i)=== null || this._storage.get(i)==undefined) {
+    this._storage.set(i, [tuple]);
   } else {
-    this._storage.set(i, _.extend(this._storage.get(i), obj));
+    var bucket = this._storage.get(i);
+    bucket.push(tuple);
+    this._storage.set(i, bucket);
   }
-  console.log(this._storage.get(i));
+
+  this._count++;
+
+  // check to see if this._storage is at least 75% full
+  // if so, increase max size
+
+  if ((this._count/this._limit) >=(3/4)) {
+    var newTable=this.resize(this._limit*2, this);
+    this._storage=newTable._storage;
+    this._limit=newTable._limit;
+    this._count=newTable._count;
+  }
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  var obj= this._storage.get(i);
-  return obj[k];
 
+HashTable.prototype.retrieve = function(k){
+  // get index
+  // assign array at index to variable bucket
+  // create new variable and assign to null
+  // loop through bucket checking to see if match in keys
+  // return result
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  var result = null;
+  _.each(bucket, function(tuple) {
+    if(tuple[0] === k) {
+      result= tuple[1];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
+  // get index
+  // get array at index assign to bucket
+  // create temporary bucket
+  // push values that do not have key to temporary bucket
+  // if tempBucket is empty return null else return tempbucket
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var obj={};
-  obj[k]=null;
-  this._storage.set(i, _.extend(this._storage.get(i), obj));
+  var bucket = this._storage.get(i);
+  var tempBucket = [];
+  _.each(bucket, function(tuple) {
+    if(tuple[0] !== k) {
+      tempBucket.push(tuple);
+    }
+  });
+
+  if(tempBucket.length === 0) {
+    this._storage.set(i, null);
+  } else {
+    this._storage.set(i, tempBucket);
+  }
+
+  // check to see if this._storage is <25% full
+  // if so, decrease size
+  this._count--;
+  if ((this._count/this._limit) <(1/4)) {
+    var newTable=this.resize(this._limit*(1/2), this);
+    this._storage=newTable._storage;
+    this._limit=newTable._limit;
+    this._count=newTable._count;
+  }
+
 };
 
+HashTable.prototype.resize=function(newLimit, that) {
+  var newTable=new HashTable(newLimit);
+  //iterate over all values in current table
+  that._storage.each(function(bucket){
+    _.each(bucket, function(tuple) {
+      newTable.insert(tuple[0], tuple[1]);
+    });
+  });
+
+  return newTable;
+};
 
 
 /*
